43f8aa0895128a3727d437bb65340557f70d2bb6
Siddhesh Kankekar <kankekas@tcd.ie>
2021-03-18 12:47:35 +0000

    App Completed

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { Text, View, ImageBackground } from "react-native";
+import { View, Text, ImageBackground } from "react-native";
 import StyledButton from "../StyledButton";
 import styles from "./styles";
 
 const CarItem = (props) => {
-  const { name, tagline, taglineCTA, image } = props;
+  const { name, tagline, taglineCTA, image } = props.car;
 
   return (
     <View style={styles.carContainer}>
@@ -12,18 +12,17 @@ const CarItem = (props) => {
 
       <View style={styles.titles}>
         <Text style={styles.title}>{name}</Text>
-        <Text style={styles.subtitle}>{tagline} 
-        <Text style={styles.subtitleCTA}>{taglineCTA}</Text>
+        <Text style={styles.subtitle}>
+          {tagline} <Text style={styles.subtitleCTA}>{taglineCTA}</Text>
         </Text>
-        
       </View>
 
-      <View style={styles.buttonContainer}>
+      <View style={styles.buttonsContainer}>
         <StyledButton
           type="primary"
           content={"Custom Order"}
           onPress={() => {
-            console.warn("CUSTOM ORDER was PRESSED");
+            console.warn("Custom Order was pressed");
           }}
         />
 
@@ -31,7 +30,7 @@ const CarItem = (props) => {
           type="secondary"
           content={"Existing Inventory"}
           onPress={() => {
-            console.warn("EXISTING INVENTORY was PRESSED");
+            console.warn("Existing Inventory was pressed");
           }}
         />
       </View>
--- components/CarItem/styles.js
+++ components/CarItem/styles.js
@@ -1,9 +1,9 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet, Dimensions } from "react-native";
 
 const styles = StyleSheet.create({
   carContainer: {
     width: "100%",
-    height: "100%",
+    height: Dimensions.get("window").height,
   },
   titles: {
     marginTop: "30%",
@@ -14,14 +14,14 @@ const styles = StyleSheet.create({
     fontSize: 40,
     fontWeight: "500",
   },
+  subtitleCTA: {
+    textDecorationLine: "underline",
+  },
   subtitle: {
     fontSize: 16,
     color: "#5c5e62",
   },
 
-  subtitleCTA: {
-    textDecorationLine: "underline",
-  },
   image: {
     width: "100%",
     height: "100%",
@@ -29,7 +29,7 @@ const styles = StyleSheet.create({
     position: "absolute",
   },
 
-  buttonContainer: {
+  buttonsContainer: {
     position: "absolute",
     bottom: 50,
     width: "100%",
