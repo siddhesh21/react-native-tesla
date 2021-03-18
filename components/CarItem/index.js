f000ec5cf0993adcdcb5a49c0d24b190e1e9c298
Siddhesh Kankekar <kankekas@tcd.ie>
2021-03-18 10:07:05 +0000

    Button Component

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Text, View, ImageBackground } from "react-native";
+import StyledButton from "../StyledButton";
 import styles from "./styles";
 
 function CarItem() {
@@ -14,6 +15,22 @@ function CarItem() {
         <Text style={styles.title}>Model S</Text>
         <Text style={styles.subtitle}>Staring at $69,420</Text>
       </View>
+
+      <StyledButton
+        type="primary"
+        content={"Custom Order"}
+        onPress={() => {
+          console.warn("CUSTOM ORDER was PRESSED");
+        }}
+      />
+
+      <StyledButton
+        type="secondary"
+        content={"Existing Inventory"}
+        onPress={() => {
+          console.warn("EXISTING INVENTORY was PRESSED");
+        }}
+      />
     </View>
   );
 }
